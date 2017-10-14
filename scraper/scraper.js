bf8fc9473424f7ad807b021882208685bba0ac12
Gregory Hogue <gfjhogue@gmail.com>
2017-10-14 11:17:29 -0400

    Added missing param to 0x subscribe call.

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -3,12 +3,13 @@
 var filterEvent = require('./util/filterEvents.js');
 var zeroEx = require('./util/zeroEx.js');
 
-zeroEx.exchange.subscribeAsync('LogFill', (evt) => {
+zeroEx.exchange.subscribeAsync('LogFill', {}, (evt) => {
+  console.log('Event', evt);
   if (filterEvent(evt)) {
     // TODO: Write to DB
   }
 }).then((result) => {
-  console.log(result);
+  console.log('Subscription token:', result);
 }).catch((err) => {
   console.error(err);
 });
