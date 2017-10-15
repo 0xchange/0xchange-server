a86f57cc6901d9627ac1401c3cdd767112b4adc1
root <gfjhogue@gmail.com>
2017-10-15 12:52:20 +0000

    Fixed seconds problem

--- server/scripts/purgeExpiredOrders.js
+++ server/scripts/purgeExpiredOrders.js
@@ -5,9 +5,10 @@ module.exports = function () {
   console.log('Running purge...');
   var promises = [];
   db.query('SELECT orderObj FROM orders').then((result) => {
+    console.log(result);
     result.rows.forEach((element) => {
-      var expiration = parseInt(element.expirationUnixTimestampSec);
-      if (expiration < Date.now()) {
+      var expiration = parseInt(element.orderobj.expirationUnixTimestampSec);
+      if (expiration < Date.now()/1000) {
         promises.push(db.query(
           'DELETE FROM orders WHERE orderObj = $1',
           [element.orderobj]
