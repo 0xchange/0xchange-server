385bbbdb86a61b70d604a590d867a83ad1c6266b
Gregory Hogue <gfjhogue@gmail.com>
2017-10-25 21:12:57 -0400

    Move error whitelist to a separate file.

--- scraper/first_scrape.js
+++ scraper/first_scrape.js
@@ -1,18 +1,9 @@
 var async = require('async-bluebird');
 var {ExchangeContract, provider} = require('./util/ethers.js');
 var processLogFill = require('./util/processLogFill.js');
+var error_whitelist = require('./util/errorWhitelist.js');
 
 var LogFill = ExchangeContract.interface.events.LogFill();
-
-var error_whitelist = {  // These errors won't stop logs processing
-  DUPLICATE_ORDER: true,
-  INSUFFICIENT_MAKER_BALANCE: true,
-  INSUFFICIENT_MAKER_FEE_BALANCE: true,
-  NOT_A_LOGFILL_FUNCTION: true,
-  NOT_FILLORDER: true,
-  ORDER_FILL_EXPIRED: true,
-  ORDER_REMAINING_FILL_AMOUNT_ZERO: true
-}
 
 
 var error_logs = {};
---
+++ scraper/util/errorWhitelist.js
@@ -0,0 +1,9 @@
+module.exports = {  // These errors won't stop logs processing
+  DUPLICATE_ORDER: true,
+  INSUFFICIENT_MAKER_BALANCE: true,
+  INSUFFICIENT_MAKER_FEE_BALANCE: true,
+  NOT_A_LOGFILL_FUNCTION: true,
+  NOT_FILLORDER: true,
+  ORDER_FILL_EXPIRED: true,
+  ORDER_REMAINING_FILL_AMOUNT_ZERO: true
+}
